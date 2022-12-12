5f6bd788cefdd1525abd268610d68c64f19c999f
ankitrana <ranaankitr@outlook.com>
2022-12-12 13:16:22 +0530

    Contact US Updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import { Box, Button, Typography, Zoom } from '@mui/material'
 import React, { useState } from 'react'
+import {details} from "../../details"
 import "./Contact.css"
 
 const Contact = () => {
@@ -32,7 +33,12 @@ const Contact = () => {
         <Typography className='contactSubHeading' sx={{textAlign:"center",width:"70%",margin:"auto",color:"rgb(124,134,164)"}} variant='h5'>I’m currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</Typography>
         
         <Button className='contactButton' onClick={mailTo} variant="outlined" sx={{border: "1px solid #64ffda",color: "white",fontSize: "0.9rem",margin:"auto",padding:"1% 3%",display:"block",marginTop:"2%"}}>Say Hello</Button>
-
+        <Box sx={{marginTop:"2%",display:"flex",justifyContent:"center",alignContent:"center",alignItems:"center",gap:"15px"}}>
+            <a className='socialLogo' target={"_blank"} href={details.githubLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-brands fa-github"></i></a>
+            <a className='socialLogo' target={"_blank"} href={details.linkedInLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-brands fa-linkedin"></i></a>
+            {/* <a className='socialLogo' target={"_blank"} href={details.instagramLink} style={{textDecoration:"none",color:"white",fontSize:"1.7rem"}}><i className="fa-brands fa-instagram"></i></a> */}
+            <a className='socialLogo' target={"_blank"} href={details.mailLink} style={{textDecoration:"none",color:"white",fontSize:"1.5rem"}}><i className="fa-regular fa-envelope"></i></a>
+        </Box>
         
     </Box>
     </Zoom>
